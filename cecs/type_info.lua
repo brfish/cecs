0af17176947c14d3280d555c7a1df25f16dd98ce
brfish <1748065414@qq.com>
2019-07-03 14:58:03 +0800

    Fix the type check

--- cecs/type_info.lua
+++ cecs/type_info.lua
@@ -1,34 +1,42 @@
 local Types = {}
 
 function Types.isComponent(obj)
+	if type(obj) ~= "table" then return false end
 	return obj.__isComponent ~= nil
 end
 
 function Types.isEntity(obj)
+	if type(obj) ~= "table" then return false end
 	return obj.__isEntity ~= nil
 end
 
 function Types.isSystem(obj)
+	if type(obj) ~= "table" then return false end
 	return obj.__isSystem ~= nil
 end
 
 function Types.isEvent(obj)
+	if type(obj) ~= "table" then return false end
 	return obj.__isEvent ~= nil
 end
 
 function Types.isEntityManager(obj)
+	if type(obj) ~= "table" then return false end
 	return obj.__isEntityManager ~= nil
 end
 
 function Types.isEventManager(obj)
+	if type(obj) ~= "table" then return false end
 	return obj.__isEventManager ~= nil
 end
 
 function Types.isWorld(obj)
+	if type(obj) ~= "table" then return false end
 	return obj.__isWorld ~= nil
 end
 
 function Types.typeinfo(obj)
+	if type(obj) ~= "table" then return type(obj) end
 	if Types.isComponent(obj) then return "component" end
 	if Types.isEntity(obj) then return "entity" end
 	if Types.isSystem(obj) then return "system" end
@@ -36,7 +44,7 @@ function Types.typeinfo(obj)
 	if Types.isWorld(obj) then return "world" end
 	if Types.isEntityManager(obj) then return "entitymanager" end
 	if Types.isEventManager(obj) then return "eventmanager" end
-	return type(obj)
+	return "table"
 end
 
 function Types.error(obj, right)
